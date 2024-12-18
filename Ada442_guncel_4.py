1fb3c990acbf20f211eaa17064e5e80267e39fa0
gulsumkayhann <ggulsumkayhann@gmail.com>
2024-12-18 18:21:20 +0300

    Update Ada442_guncel_4.py

--- Ada442_guncel_4.py
+++ Ada442_guncel_4.py
@@ -8,7 +8,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import joblib
 from matplotlib import pyplot as plt
 from sklearn.feature_selection import mutual_info_classif, SelectKBest
 from sklearn.preprocessing import MinMaxScaler
