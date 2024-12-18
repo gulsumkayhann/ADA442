f7a4bf90348a901e985da0c5147fc37e81887b5c
gulsumkayhann <ggulsumkayhann@gmail.com>
2024-12-18 21:56:51 +0300

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -114,6 +114,7 @@ if selected_section == "4. Model Selection":
     st.header("4. Model Selection")
 
     # Ensure that important_features is defined before using it
+     corr_matrix = numeric_data.corr()
     important_features = corr_matrix.index[abs(corr_matrix['y']) > threshold].tolist()
     X = data[important_features]
     y = data['y']
